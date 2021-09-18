4a908a1e4d418124a8733245223b059801e31c63
kshitij <kshtj21@hotmail.com>
2021-09-18 22:40:07 +0530

    other commit

--- src/Transactions.js
+++ src/Transactions.js
@@ -2,24 +2,18 @@ import React, {useState} from 'react';
 import {AiOutlineCloseCircle} from "react-icons/all";
 
 function Transactions(props) {
-    const [dltButton, setdltButton] = useState({display: 'none'});
+    // const [dltButton, setdltButton] = useState({display: 'none'});
     const deleteTransaction = (i) => {
-        props.deleteTransaction(prev => prev.splice(i, 1))
+        props.deleteTransaction(prev => prev.filter((tr, index) => index !== i))
     }
     return (
         <div className="balance">
             <span className="history">History</span>
             <hr/>
-            <ul>{props.transactions.map((t, index) => (
+            <ul className="ul-remove">{props.transactions.map((t, index) => (
                 <li className={`transaction ${t.amount > 0 ? "transaction-border-income" : "transaction-border-expense"}`}
-                    key={index}
-                    onMouseEnter={e => {
-                        setdltButton({display: 'inline-block'});
-                    }}
-                    onMouseLeave={e => {
-                        setdltButton({display: 'none'})
-                    }}>
-                    <div className="delete-tran" style={dltButton} onClick={() => deleteTransaction(index)}>
+                    key={index}>
+                    <div className="delete-tran" onClick={() => deleteTransaction(index)}>
                         <AiOutlineCloseCircle/>
                     </div>
                     <div className="tran-name">
